cea42ec411dac5e7fe6ef840da98cf0880761031
GonzaloPereda <63264439+GonzaloPereda@users.noreply.github.com>
2021-02-04 23:56:02 -0300

    guardo mi array de transacciones en formato json en el local storage

--- app.js
+++ app.js
@@ -3,26 +3,51 @@ const form = document.getElementById("transaccionFormulario");
 form.addEventListener("submit", function (event) {
   event.preventDefault(); // Detiene el envio del evento que hace que no se recargue la pagina
   let transactionFormData = new FormData(form); // Obtiene los datos del formulario
+  let transactionObject = convertFormDataToTransactionObject(
+    transactionFormData
+  );
+  saveTransactionObject(transactionObject);
+  insertRowTransactionTable(transactionObject);
 });
 
-function insertRowTransactionTable(
-  transaccionType,
-  transaccionDescription,
-  transaccionAmount,
-  transaccionCategory
-) {
+function convertFormDataToTransactionObject(transactionFormData) {
+  let transactionType = transactionFormData.get("transaccionType");
+  let transactionDescription = transactionFormData.get(
+    "transaccionDescription"
+  ); // Toma el name del html y lo guarda en la tabla
+  let transactionAmonut = transactionFormData.get("transaccionAmount");
+  let transactionCategory = transactionFormData.get("transaccionCategory");
+  return {
+    transaccionType: transactionType,
+    transaccionDescription: transactionDescription,
+    transaccionAmonut: transactionAmonut,
+    transaccionCategory: transactionCategory,
+  };
+}
+
+function insertRowTransactionTable(transactionObject) {
   let tabla = document.getElementById("transaccionTable"); // Obtiene los datos de la tabla, una referencia
   let newTransactionRow = tabla.insertRow(-1); // A esta tabla le agrega una nueva fila
 
   let newTypeCell = newTransactionRow.insertCell(0); // y esta nueva fila le agrega una nueva celda en la posicion 0
-  newTypeCell.textContent = transactionFormData.get("transaccionType");
+  newTypeCell.textContent = transactionObject["transaccionType"];
 
   newTypeCell = newTransactionRow.insertCell(1);
-  newTypeCell.textContent = transactionFormData.get("transaccionDescription"); // Toma el name del html y lo guarda en la tabla
+  newTypeCell.textContent = transactionObject["transaccionDescription"]; // Toma el name del html y lo guarda en la tabla
 
   newTypeCell = newTransactionRow.insertCell(2);
-  newTypeCell.textContent = transactionFormData.get("transaccionAmount");
+  newTypeCell.textContent = transactionObject["transaccionAmount"];
 
   newTypeCell = newTransactionRow.insertCell(3);
-  newTypeCell.textContent = transactionFormData.get("transaccionCategory");
+  newTypeCell.textContent = transactionObject["transaccionCategory"];
+}
+
+function saveTransactionObject(transactionObject) {
+  let myTransactionArray =
+    JSON.parse(localStorage.getItem("transactionData")) || [];
+  myTransactionArray.push(transactionObject);
+  // Convierto mi array de transacciones a json
+  let transactionArrayJson = JSON.stringify(myTransactionArray);
+  // guardo mi array de transaccion en formato Json en el local storage
+  localStorage.setItem("transactionData", transactionArrayJson);
 }
